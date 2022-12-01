755b3129c84980daaece2dbe8d125a5426b25e6c
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-01 23:40:48 +0300

    Add files via upload

---
+++ array_list/SortByDescending.cs
@@ -0,0 +1,37 @@
+﻿
+
+//Iterative way
+using System;
+class Program
+{
+
+    public static void Main()
+    {
+        // declaring and initializing the array
+        int[] arr = new int[] { 0, 15, 100, 66, 7, 1 };
+
+        int temp;
+
+        // traverse 0 to array length
+        for (int i = 0; i < arr.Length - 1; i++)
+
+            // traverse i+1 to array length
+            for (int j = i + 1; j < arr.Length; j++)
+
+                // compare array element with all next element
+
+                if (arr[i] < arr[j])
+                {
+
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+
+         // print all element of array
+        foreach (int value in arr)
+        {
+            Console.Write(value + " ");
+        }
+    }
+}
