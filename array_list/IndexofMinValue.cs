0db9554f7e5ac82ff9a03253a84ab6f5924f5b8a
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-02 16:10:57 +0300

    Add files via upload

---
+++ array_list/IndexofMinValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IndexofMinValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] anArray = { 23, 22, 120, 6 };
+            int m = anArray.Min();
+            int p = Array.IndexOf(anArray, m);
+            Console.WriteLine(p);
+
+        }
+    }
+}
