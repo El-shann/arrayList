b0e9b6e9ec52f81d0df32961c0422c63d88f024b
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-02 16:35:59 +0300

    Add files via upload

---
+++ array_list/GetSetbyIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+class GetAndSetbyIndex
+{
+
+    
+    public static void Main()
+    {
+
+        
+        ArrayList myList = new ArrayList();
+
+        
+        myList.Add("A");
+        myList.Add("B");
+        myList.Add("C");
+       
+
+       
+        foreach (string str in myList)
+        {
+            Console.WriteLine(str);
+        }
+
+        Console.WriteLine("After Item[int32] Property: ");
+
+        
+        myList[2] = "Z";
+
+        
+        foreach (string str in myList)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
