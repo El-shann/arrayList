b4bf09aa75881b5f10ec0b0ada16e45b5a856c7e
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-02 16:20:54 +0300

    Add files via upload

---
+++ array_list/ListLength.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class listLength
+{
+
+    
+    public static void Main()
+    {
+
+       
+        List<int> firstlist = new List<int>();
+
+        
+        for (int i = 4; i < 8; i++)
+        {
+            firstlist.Add(i * 2);
+        }
+
+       
+        Console.WriteLine(firstlist.Count);
+    }
+}
