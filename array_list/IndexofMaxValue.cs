2114af685d092c29ce8c7487ab4eceafb5942bab
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-02 16:05:24 +0300

    Add files via upload

---
+++ array_list/IndexofMaxValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MinMaxofArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] anArray = { 0, 17, 233, 5 };
+            int m = anArray.Max();
+            int p = Array.IndexOf(anArray, m);
+            Console.WriteLine(p);
+
+        }
+    }
+}
