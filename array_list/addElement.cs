1f15a1208c5fbb7b2f657a37952323e13b1c9a27
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-02 16:42:42 +0300

    Add files via upload

---
+++ array_list/addElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class program
+{
+    public static void Main()
+    {
+        List<string> countries = new List<string>() { "Combodia", "lesoto", "Morocco", "Malta" };
+        string country = "India";
+
+        countries.Insert(countries.Count, country);
+
+        Console.WriteLine(String.Join(", ", countries));        
+    }
+}
