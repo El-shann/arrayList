418537f01c7537da510117823a8feb07094fe5f4
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-02 16:47:54 +0300

    Add files via upload

---
+++ array_list/add element.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class listLength
+{
+    public static void Main()
+    {
+        List<int> values = new List<int>() { 0, 117, 55, 1001 };
+        int item = 4;
+
+        values.Insert(0, item);  //
+
+        Console.WriteLine(String.Join(", ", values));
+    }
+}
+
